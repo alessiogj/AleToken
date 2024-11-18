1f871fe4a24b5395f95fbd6879f238e071eab42c
alessiogj <alessiovr1@gmail.com>
2024-11-18 18:59:37 +0100

    Modificata procedura di deploy e script

--- ignition/modules/AleTokenModule.js
+++ ignition/modules/AleTokenModule.js
@@ -1,10 +1,8 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("AleTokenModule", (m) => {
-  // Parametri del modulo
   const initialSupply = m.getParameter("initialSupply", 1_000_000n);
 
-  // Distribuisci il contratto AleToken
   const aleToken = m.contract("AleToken", [initialSupply]);
 
   return { aleToken };
